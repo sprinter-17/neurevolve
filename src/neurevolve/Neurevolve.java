3cf2125c82063288b465ca09ddfff549c1cdee7c
simon <simon@10.0.0.149>
2017-01-29 00:50:16 -0500

    Initial project creation

---
+++ src/neurevolve/Neurevolve.java
@@ -0,0 +1,8 @@
+package neurevolve;
+
+public class Neurevolve {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
---
+++ test/neurevolve/NeurevolveTest.java
@@ -0,0 +1,11 @@
+package neurevolve;
+
+import org.junit.Test;
+
+public class NeurevolveTest {
+
+    @Test
+    public void testMain() {
+    }
+
+}
